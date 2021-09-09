a41d76683429594f1f9d8e2ae2bd1e361cfe5ea7
Darío Fernández <dariofedes@Darios-MacBook-Pro.local>
2021-09-09 11:26:49 +0200

    add two by to grid with trhee alive cells

--- src/main/java/com/gameoflife/gameoflife/GameOfLife.java
+++ src/main/java/com/gameoflife/gameoflife/GameOfLife.java
@@ -12,6 +12,22 @@ public class GameOfLife {
 	}
 
 	public boolean[][] nextGen() {
+		int aliveCells = 0;
+
+		for(boolean [] row : boardState) {
+			for(boolean cell : row) {
+				if(cell) aliveCells++;
+			}
+		}
+
+		if(aliveCells == 3) return boardState;
+		
+		for (int i = 0; i < boardState.length; i++) {
+			for (int j = 0; j < boardState[i].length; j++) {
+				boardState[i][j] = false;
+			}
+		}
+
 		return boardState;
 	}
 }
--- src/test/java/com/gameoflife/gameoflife/GameOfLifeShould.java
+++ src/test/java/com/gameoflife/gameoflife/GameOfLifeShould.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class GameOfLifeShould {
@@ -21,5 +20,48 @@ class GameOfLifeShould {
 		boolean [][] currentState = target.nextGen();
 		//then
 		assertFalse(currentState[0][0]);
+	}
+
+	@Test
+	void contain_single_dead_cell_if_a_single_alive_cell_is_passed() {
+		//given
+		boolean [][] seedState = {{true}};
+		target = new GameOfLife(seedState);
+		//when
+		boolean [][] currentState = target.nextGen();
+		//then
+		assertFalse(currentState[0][0]);
+	}
+
+	@Test
+	void contain_four_dead_cells_if_2_x_2_grid_with_one_alive() {
+		//given
+		boolean [][] seedState = {{false, false}, {false, true}};
+		target = new GameOfLife(seedState);
+
+		//when
+		boolean [][] currentState = target.nextGen();
+
+		//then
+		assertFalse(currentState[0][0]);
+		assertFalse(currentState[0][1]);
+		assertFalse(currentState[1][0]);
+		assertFalse(currentState[1][1]);
+	}
+
+	@Test
+	void contain_three_alive_cells_if_2_x_2_grid_with_three_alive() {
+		//given
+		boolean [][] seedState = {{false, true}, {true, true}};
+		target = new GameOfLife(seedState);
+
+		//when
+		boolean [][] currentState = target.nextGen();
+
+		//then
+		assertFalse(currentState[0][0]);
+		assertTrue(currentState[0][1]);
+		assertTrue(currentState[1][0]);
+		assertTrue(currentState[1][1]);
 	}
 }
