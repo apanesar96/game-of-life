41aa42383081ec98a9e8c9cb5e40f6d9a4e52340
Aman Panesar <amanpanesar@Amans-MacBook-Pro.local>
2021-09-09 09:19:17 +0100

    initial commit

---
+++ src/test/java/com/gameoflife/gameoflife/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameoflife.gameoflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
