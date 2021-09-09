e34980f5fa76917261276859f2676fcfd9dc428e
SteeleSR <samuelrsteele@gmail.com>
2021-09-09 10:59:34 +0100

    Created methods to count living neighbours and check if an array of possible neighbours are within the confines of the grid

--- src/main/java/com/gameoflife/gameoflife/GameOfLife.java
+++ src/main/java/com/gameoflife/gameoflife/GameOfLife.java
@@ -11,6 +11,34 @@ public class GameOfLife {
 		boardState = seedState;
 	}
 
+	private int countLivingNeighbours(boolean[] neighbours) {
+		int livingNeighbours = 0;
+
+	}
+
+	private boolean[] getNeighbours(int 0, int 0) {
+
+		boolean[] neighbouringCells = {
+				{boardState[-1][-1]},
+				{boardState[-1][0]},
+				{boardState[-1][+1]},
+				{boardState[0][-1]},
+				{boardState[0][1]},
+				{boardState[+1][-1]},
+				{boardState[+1][0]},
+				{boardState[+1][+1}};
+
+//				for() {
+//					if(isWithinGrid(cell) && cell == true) {
+//						livingNeighbours++;
+//					}
+//				}
+	}
+
+	private boolean isWithinGrid(int x, int y) {
+		return (y >= 0 && y <= boardState.length) && (x >= 0 && x <= boardState[0].length);
+	}
+
 	public boolean[][] nextGen() {
 		int aliveCells = 0;
 
